f9d831109bbaf3ce4e33ad94f03e0eea2e5471c8
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-21 16:58:50 +0100

    Ajout des methodes findAll et findById pour la classe Personne

--- src/activeRecord/Personne.java
+++ src/activeRecord/Personne.java
@@ -1,4 +1,66 @@
 package activeRecord;
 
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Personne {
+    private int id;
+    private String nom;
+    private String prenom;
+
+    public Personne(String pNom, String pPren)
+    {
+        this.nom = pNom;
+        this.prenom = pPren;
+        this.id = -1;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public static ArrayList<Personne> findAll() throws SQLException {
+        ArrayList<Personne> arrayList = new ArrayList<>();
+        Connection connection = DBConnection.getConnection();
+        String sql = "SELECT * FROM personne ";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while (resultSet.next())
+        {
+            int id = resultSet.getInt(1);
+            String nom = resultSet.getString(2);
+            String prenom = resultSet.getString(3);
+            Personne p = new Personne(nom, prenom);
+            p.setId(id);
+
+            arrayList.add(p);
+        }
+        connection.close();
+
+        return arrayList;
+    }
+
+    public static Personne findById(int id) throws SQLException {
+        Personne res = null;
+        String sql = "SELECT * FROM personne WHERE ID = ?";
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next())
+        {
+            int idP = resultSet.getInt(1);
+            String nom = resultSet.getString(2);
+            String prenom = resultSet.getString(3);
+            res = new Personne(nom, prenom);
+            res.setId(idP);
+        }
+        return res;
+    }
+
+
 }
