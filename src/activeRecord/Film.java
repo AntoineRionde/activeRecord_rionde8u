f58e00b6289e0989041db3371c36d1872b6821af
toinou887 <rionde8u@etu.univ-lorraine.fr>
2022-11-24 12:02:00 +0100

    Ajout des attributs, des constructeurs et de la methode findById

--- src/activeRecord/Film.java
+++ src/activeRecord/Film.java
@@ -1,4 +1,45 @@
 package activeRecord;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Film {
+    private String titre;
+    private int id; // id du film
+    private int id_real; // id du realisateur
+
+    public Film(String titre, Personne realisateur)
+    {
+        this.titre = titre;
+        this.id_real = realisateur.getId();
+        this.id = -1;
+    }
+
+    private Film(int id, int id_real, String titre)
+    {
+        this.id = id;
+        this.id_real = id_real;
+        this.titre = titre;
+    }
+
+    public Film findById(int id) throws SQLException {
+        Film film = null;
+        String sql = "SELECT * FROM Film WHERE id = ?";
+        Connection connection = DBConnection.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.getResultSet();
+        while (resultSet.next())
+        {
+            int id2 = resultSet.getInt(1);
+            String titre = resultSet.getString(2);
+            int id_real = resultSet.getInt(3);
+            film = new Film(id2, id_real, titre);
+        }
+        return film;
+    }
+
+
 }
